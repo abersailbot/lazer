d0b2362183c5326e5c8955d0aec46c9dd709a826
brunskins <62477802+brunskins@users.noreply.github.com>
2024-04-24 16:17:25 +0100

    Update global_pos.py
    documentation

--- pi/global_pos.py
+++ pi/global_pos.py
@@ -1,9 +1,9 @@
-def gps_loc():
+def gps_loc(): # if having consistancy problems run gpsd in background
     import gps
     session=gps.gps(mode=gps.WATCH_ENABLE)
     lat=""
     lon=""
-    for i in range(1,10):
+    for i in range(1,10): # currently running at 10 times to ensue data is collected
         report=session.next()
         report2=str(report)
         print(report2[10])
@@ -13,9 +13,9 @@ def gps_loc():
         lat=""
         lon_start=0
         lon_end=0
-        lon=""
+        lon="" #above is all initiation
         try:
-            lat_start=report2.index("'lat': ")
+            lat_start=report2.index("'lat': ") # report2 outputs as a long data string the try and expect finds the start and end of usefull data in the string
             print("found")
             lat_end=report2.find(",",lat_start)
             print(lat_start,"to",lat_end)
@@ -24,9 +24,9 @@ def gps_loc():
         for i in range((lat_start+7),lat_end):
             temp=report2[i]
             
-            lat=lat+temp
+            lat=lat+temp # adds individual numbers to string via concatination
             
-        try:
+        try: # here starts the repeat of the above but for lon (longitude)
             lon_start=report2.index("'lon': ")
             print("found")
             lon_end=report2.find(",",lon_start)
@@ -38,9 +38,9 @@ def gps_loc():
             
             lon=lon+temp
             
-        print(lat)
+        print(lat) # test printing cases for debug 
         print(lon)
-    lat=float(lat)
+    lat=float(lat) # converts from the sting for concatinaion to float 
     lon=float(lon)
-    return lat, lon
+    return lat, lon # returns data this file is desighned to be used as a a subroutine 
 
